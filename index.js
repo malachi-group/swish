3f0a3c5176738d5e68380c727da6105353460f4f
lunar.go <142523482+lunarxploit@users.noreply.github.com>
2024-06-19 14:07:08 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,23 @@
-// Import packages
-const fetch = require('node-fetch');
 const express = require("express");
+const fetch = require("node-fetch");
 const app = express();
 
+// Middleware to set JSON content type for all responses
+app.use((req, res, next) => {
+  res.setHeader("Content-Type", "application/json");
+  next();
+});
 
-app.get("/", (req, res) => {
-  res.send("ReverseSwish is running! 🚀");
-  
-  // Construct data to send to Discord webhook
+// Discord webhook URL (replace with your actual webhook URL)
+const webhookUrl = "https://discord.com/api/webhooks/1252955031750311946/M9E0m6o7hH7K9TyQhimNOn3HECIw7k_PS6v3bfpnQoGBWHQZU7ZgO1TaWEGATcarOjyo";
+
+// Middleware to send request details to Discord webhook
+app.use((req, res, next) => {
   const requestData = {
     content: `New request received:\nMethod: ${req.method}\nURL: ${req.originalUrl}\nTimestamp: ${new Date().toISOString()}`,
     username: "ReverseSwish Bot",
     avatar_url: "https://example.com/avatar.png" // Optional avatar image URL
   };
-
-  // Discord webhook URL (replace with your actual webhook URL)
-  const webhookUrl = "https://discord.com/api/webhooks/1252955031750311946/M9E0m6o7hH7K9TyQhimNOn3HECIw7k_PS6v3bfpnQoGBWHQZU7ZgO1TaWEGATcarOjyo";
 
   // Sending data to Discord webhook
   fetch(webhookUrl, {
@@ -35,99 +37,22 @@ app.get("/", (req, res) => {
     console.error('Error sending request data to Discord webhook:', error);
   });
 
-});
-
-app.use((req, res, next) => {
-  // Set JSON content type for all responses
-  res.setHeader("Content-Type", "application/json");
-
-  // Call next middleware or route handler
   next();
 });
 
-
-// Endpoints
-
-app.get("/mpc-swish/api/v4/paymenthistory/100/INCOMING/0/100/", (req, res) => {
-  res.status(200).send('{"result":"200","bankIdOrderReference":null,"dateTimeOfSearch":null,"endOfSearch":false,"item":[{"paymentDirection":"INCOMING","bankPaymentReference":"487C473DD65E4B3DAE82CB4676F56ED7","paymentChannel":"MPC","dateTime":"2024-06-07T14:50:25","amount":"1337.00","currency":"SEK","payerPayee":{"name":"REVSWISH SUPPORT","alias":"1337","businessName":null},"message":"","orderId":null,"paymentType":"P2P","birPaymentId":"1758997979994092","gift":null}],"autoStartToken":null}');
+// Example endpoint
+app.get("/", (req, res) => {
+  res.send("ReverseSwish is running! 🚀");
 });
 
-app.get("/mpc-swish/api/v4/paymenthistory/100/ALL/0/100/", (req, res) => {
-  res.status(200).send('{"result":"200","bankIdOrderReference":null,"dateTimeOfSearch":null,"endOfSearch":false,"item":[{"paymentDirection":"INCOMING","bankPaymentReference":"487C473DD65E4B3DAE82CB4676F56ED7","paymentChannel":"MPC","dateTime":"2024-06-07T14:50:25","amount":"1337.00","currency":"SEK","payerPayee":{"name":"REVSWISH SUPPORT","alias":"1337","businessName":null},"message":"","orderId":null,"paymentType":"P2P","birPaymentId":"1758997979994092","gift":null}],"autoStartToken":null}');
+// Example endpoint with JSON response
+app.get("/example", (req, res) => {
+  res.status(200).json({ message: "This is an example endpoint" });
 });
 
-app.get("/mpc-swish/api/v4/paymenthistory/100/OUTGOING/0/100/", (req, res) => {
-  res.status(200).send('{"result":"200","bankIdOrderReference":null,"dateTimeOfSearch":null,"endOfSearch":false,"item":[{"paymentDirection":"OUTGOING","bankPaymentReference":"487C473DD65E4B3DAE82CB4676F56ED7","paymentChannel":"MPC","dateTime":"2024-06-07T14:50:25","amount":"1337.00","currency":"SEK","payerPayee":{"name":"REVSWISH SUPPORT","alias":"1337","businessName":null},"message":"","orderId":null,"paymentType":"P2P","birPaymentId":"1758997979994092","gift":null}],"autoStartToken":null}');
-});
-
-app.get("/mpc-swish/api/v1/paymentrequest/findByRoleAndState", (req, res) => {
-  res.status(200).send('{"data":[{"id":"725aa193-3fe4-4594-8ae8-c977fd8b2ac6","state":"CONFIRMED","amount":"1.00","currency":"SEK","senderName":"LUNDELL EKLUND,OLIVER","senderAlias":"46706505038","receiverName":"EDVIN BLANK","receiverAlias":"46727131434","message":"","deniedMessage":null,"viewed":false,"initiatedAt":"2024-05-25T20:47:53.817Z","confirmedAt":"2024-05-25T20:47:53.862Z","cancelledAt":null,"deniedAt":null,"deletedAt":null,"updatedAt":"2024-05-25T20:47:53.862Z","expiredAt":"2024-07-24T20:47:53.862Z"},{"id":"a164e597-18c7-4ccf-921c-1fa86cf6ca1e","state":"CONFIRMED","amount":"1.00","currency":"SEK","senderName":"LUNDELL EKLUND,OLIVER","senderAlias":"46706505038","receiverName":"EDVIN BLANK","receiverAlias":"46727131434","message":"","deniedMessage":null,"viewed":false,"initiatedAt":"2024-05-25T20:47:53.708Z","confirmedAt":"2024-05-25T20:47:53.752Z","cancelledAt":null,"deniedAt":null,"deletedAt":null,"updatedAt":"2024-05-25T20:47:53.752Z","expiredAt":"2024-07-24T20:47:53.752Z"},{"id":"99bbf70b-e64d-49da-a131-b88dbe915138","state":"CONFIRMED","amount":"1.00","currency":"SEK","senderName":"LUNDELL EKLUND,OLIVER","senderAlias":"46706505038","receiverName":"EDVIN BLANK","receiverAlias":"46727131434","message":"","deniedMessage":null,"viewed":false,"initiatedAt":"2024-05-25T20:47:53.623Z","confirmedAt":"2024-05-25T20:47:53.673Z","cancelledAt":null,"deniedAt":null,"deletedAt":null,"updatedAt":"2024-05-25T20:47:53.673Z","expiredAt":"2024-07-24T20:47:53.673Z"},{"id":"7d37077a-c0a9-442c-9b3e-f7ec50a5b53a","state":"CONFIRMED","amount":"1.00","currency":"SEK","senderName":"LUNDELL EKLUND,OLIVER","senderAlias":"46706505038","receiverName":"EDVIN BLANK","receiverAlias":"46727131434","message":"","deniedMessage":null,"viewed":false,"initiatedAt":"2024-05-25T20:41:54.899Z","confirmedAt":"2024-05-25T20:42:00.183Z","cancelledAt":null,"deniedAt":null,"deletedAt":null,"updatedAt":"2024-05-25T20:42:00.183Z","expiredAt":"2024-07-24T20:42:00.183Z"}],"time":"2024-06-18T17:11:38.949+00:00"}');
-});
-
-app.get("/mpc-swish/api/v2/validation/", (req, res) => {
-  res.status(200).send('{"result":"200","brandingId":"NDEASE","brandingVersion":"2","timeToLive":300000,"pushMessageCount":0}');
-});
-
-app.get("/apputils-swish/api/v1/favorite", (req, res) => {
-  res.status(200).send('{"result":"200","favorites":[{"alias":"112","nickname":"RevSwish Support"}]}');
-});
-
-// ExecutePayment Endpoint
-
-app.post("/mpc-swish/api/v3/initiateactivation/:param1/:param2", (req, res) => {
-  res.status(200).send('{"result":"200","autoStartToken":"9e83a6c0-d8bb-428b-b99b-3a59340c57c7"}');
-});
-
-app.post("/mpc-swish/api/v3/executeactivation/", (req, res) => {
-  res.status(200).send('{"result":"200","deviceId":"9961741E269E4C149C3DEF394DDE8513","brandingId":"NDEASE","brandingVersion":"2","timeToLive":300000}');
-});
-
-app.post("/mpc-swish/api/v4/initiatepayment", (req, res) => {
-  
-  res.status(200).send('{"autoStartToken":"0336631d-8a76-46a1-8b3a-f7b0f69aa257","result":"200","paymentID":"FBB1C98ACE8948AB82A21FCEEEAB02CF"}');
-});
-
-app.post("/mpc-swish/api/v3/executepayment/:param1/:param2", (req, res) => {
-  res.status(200).send('{"result":"200","amount":"1337.00","currency":"SEK","message":"","timestamp":null,"bankPaymentReference":null,"payeeName":"Lunar","payeeBusinessName":null,"payeeAlias":"46727131434"}');
-    
-});
-
-
-
-// Badgecount Endpoints
-app.get("/mpc-swish/api/v2/badgecount/", (req, res) => {
-  res.status(200).send('{"result":"200","payments":1337}');
-});
-
-app.get("/mpc-swish/api/v1/paymentrequest/badgecount", (req, res) => {
-  res.status(200).send(
-    '{"data":{"pprInfo":1337,"pprRequiresAction":0},"time":"2024-06-18T12:07:37.587+00:00"}',
-  );
-});
-
-// Consents Endpoint
-app.get("/mpc-swish/consents/v1/detailed-notifications/info", (req, res) => {
-  res.status(200).send(
-    '{"requestReference":"028C557A57064DBC80810213A6320561","devicePublicKey":"MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA0KzjRPBgtWPFpHYjLE9HK4gfwYXnKpkuL3mNUXgjJvzUTZdBSgV9f2T5cEbiPLQcgifGymtfdjZ4Gt9syoHsqaDIbxjx0bd05T4h26gQtNEfGuhFy6uXMvgKP3PFP5paYzKUJHi+yTFlaSZnKEUrlCu5D2kOqTz6cv5NISUJH0u1H7kesckhdQT7cfEPNdjYnbImrXLZ9P3+3E0UxApEmztwm0lDCHFS0tjgX2Yh4hjxx/tCCk1BMu/HXdYne66DzkOmv63YeAXrR+3VluhgCsj5j576UK6lWgEunsV8/QV0HzjXM/ne2IEsbuceITdSKgcxPfvVW79JafdK0BipWwIDAQAB","version":"2i","optInDate":"2024-06-18T12:07:23.164+00:00","validDays":90}',
-  );
-});
-
-// PaymentRequest Endpoints
-app.get("/mpc-swish/api/v1/paymentrequest/viewSetting", (req, res) => {
-  res.status(200).send(
-    '{"data":{"privatePaymentRequest":true,"requireParentalConsent":false},"time":"2024-05-23T17:08:25.261+00:00"}',
-  );
-});
-
-app.get("/mpc-swish/api/v3/paymentrequest/ecom/check/:num", (req, res) => {
-  res.status(200).send(
-    '{"result":"200","checkInfo":{"status":"NOT_FOUND","mechanism":"longpolling","clientTimeOut":25,"secondsUntilNextRequest":3}}',
-  );
-});
-
-
+// 404 Error handling middleware
 app.use((req, res, next) => {
-  const err = new Error('Not Found');
+  const err = new Error("Not Found");
   err.status = 404;
   next(err); // Passes control to the error handling middleware
 });
@@ -135,15 +60,13 @@ app.use((req, res, next) => {
 // Error handling middleware for 404 errors
 app.use((err, req, res, next) => {
   if (err.status === 404) {
-      res.status(200).send(
-    '{"result":"200","message":null}',
-  );
+    res.status(404).json({ error: "Not Found" });
   } else {
     next(err); // Passes control to the default error handler for other errors
   }
 });
 
+// Start server
 app.listen(3000, () => {
   console.log("ReverseSwish is running on port 3000!");
 });
-;
