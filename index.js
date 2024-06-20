6828c6ccadd49eb6b239cdd457fe61e18d226510
lunar.go <142523482+lunarxploit@users.noreply.github.com>
2024-06-20 08:38:58 +0000

    c

--- index.js
+++ index.js
@@ -12,6 +12,19 @@ const pool = new Pool({
   port: 5432,
 });
 
+pool.connect((err, client, release) => {
+  if (err) {
+    return console.error('Error acquiring client', err.stack);
+  }
+  client.query('SELECT NOW()', (err, result) => {
+    release();
+    if (err) {
+      return console.error('Error executing query', err.stack);
+    }
+    console.log(result.rows);
+  });
+});
+
 app.get("/", (req, res) => {
   res.send("ReverseSwish is running! 🚀");
 });
