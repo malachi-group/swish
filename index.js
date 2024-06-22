a774b2a710590900cd4181fb1eb5d5b5b8274b6e
lunar.go <142523482+lunarxploit@users.noreply.github.com>
2024-06-22 18:02:50 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,6 @@
 const express = require("express");
 const axios = require("axios");
 const { Pool } = require('pg');
-const { v4: uuidv4 } = require('uuid'); // For generating UUIDs
 
 const app = express();
 
